3e18dbb500f0c0094e01116009d61e04c24263c6
Ganindu Pushpakumara <118869670+pushpe1174@users.noreply.github.com>
2023-02-14 16:56:14 +0530

    Only Emulator Camera Screen
    Not working on real device working on it

--- currency_mate_app/lib/Screens/camera_screen.dart
+++ currency_mate_app/lib/Screens/camera_screen.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
 import 'package:image_picker/image_picker.dart';
 
 import '../Utils/style.dart';
@@ -15,9 +16,14 @@ class DetectCurrency extends StatefulWidget {
 class _DetectCurrencyState extends State<DetectCurrency> {
 
   File? pickedImage;
+
   Future<void> _getImgFromCamera() async{
     final ImagePicker picker = ImagePicker();
-    final XFile? photo = await picker.pickImage(source: ImageSource.camera);
+    final XFile? photo = await picker.pickImage(
+      source: ImageSource.camera,
+      maxWidth: 1080,
+      maxHeight: 1080,
+    );
 
     setState(() {
       pickedImage = File(photo!.path);
@@ -29,7 +35,6 @@ class _DetectCurrencyState extends State<DetectCurrency> {
   void initState() {
     // TODO: implement initState
     super.initState();
-    _getImgFromCamera();
   }
 
   @override
@@ -44,13 +49,48 @@ class _DetectCurrencyState extends State<DetectCurrency> {
           backgroundColor: const Color(0xff1D3557),
           elevation: 0,
         ),
-        body: Column(
-          children: [
-            pickedImage != null ?
-            Image.file(pickedImage!) :
-            const Text("No image selected")
-          ],
-        )
+       body: Column(
+         children: [
+           pickedImage == null?
+               Center(
+                 child: ElevatedButton(
+                   onPressed: (){
+                     _getImgFromCamera();
+                   },
+                   child: Text("CLick me"),
+                 ),
+               ):
+               Column(
+                 children: [
+                   Container(
+                     width: MediaQuery.of(context).size.width,
+                     height: MediaQuery.of(context).size.width,
+                     child: Image.file(
+                         pickedImage!,
+                       fit: BoxFit.contain,
+                     ),
+                   ),
+                   const Gap(20),
+                   ElevatedButton.icon(
+                     onPressed: () {  },
+                     icon: const Icon(
+                       Icons.check,
+                       size: 50,
+                     ),
+                     label: const Text(
+                       "Proceed",
+                       style: TextStyle(
+                         fontSize: 30,
+                       ),
+                     ),
+                     style: ElevatedButton.styleFrom(
+                         padding: const EdgeInsets.fromLTRB(20, 10, 20, 10)
+                     ),
+                   ),
+                 ],
+               )
+         ],
+       ),
     );
   }
 }

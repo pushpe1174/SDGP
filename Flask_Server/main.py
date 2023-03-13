bc641a906f4d8ae3967bd9707bc5710eb9e84172
Ganindu Pushpakumara <118869670+pushpe1174@users.noreply.github.com>
2023-03-14 01:33:23 +0530

    Local port change

--- Flask_Server/main.py
+++ Flask_Server/main.py
@@ -1,11 +1,37 @@
 import os
 import werkzeug
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from mlmodel import getPredict
 
 app = Flask(__name__)
 
 ALLOW_FLIE_TYPE = {'png','jpg','jpeg'}
+
+# @app.route('/', methods=['GET'])
+# def welcome():
+#     return "hello world"
+
+# def is_allowed(filename):
+#     return '.' in filename and filename.rsplit('.',1)[1].lower() in ALLOW_FLIE_TYPE
+
+# @app.route('/upload', methods=['POST'])
+# def predictfn ():
+#     if 'file' not in request.files:
+#         return jsonify({'error': 'no-file'})
+
+#     file = request.files['file']
+
+#     if file.filename == '':
+#         return jsonify({'error': 'no-filename'})
+
+#     if not is_allowed(file.filename):
+#         return jsonify({'error': 'not-a-valid-file-type'})
+
+#     # perform prediction
+#     prediction = getPredict(file.filename)
+
+#     # return prediction result
+#     return prediction
 
 @app.route('/upload', methods = ['POST'])
 def upload():
@@ -21,6 +47,6 @@ def upload():
 
         # return prediction result
         return prediction
-
+    
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=4000)
--- currency_mate_app/lib/main.dart
+++ currency_mate_app/lib/main.dart
@@ -5,7 +5,6 @@ import 'package:currency_mate_app/Screens/loading_screen.dart';
 import 'package:currency_mate_app/Screens/login_screen.dart';
 import 'package:currency_mate_app/Screens/record_screen.dart';
 import 'package:currency_mate_app/Screens/sign_in_screen.dart';
-import 'package:currency_mate_app/Screens/summary_screen.dart';
 import 'package:currency_mate_app/Utils/style.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';

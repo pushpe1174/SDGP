38e8b678f4eeec1ab6f51ae5348cf27ac6ac9abd
Dilanka Yasith <123799744+DilankaYasith@users.noreply.github.com>
2023-03-25 20:40:02 +0530

    Updated the voice feature

--- currency_mate_app/lib/Screens/summary_screen.dart
+++ currency_mate_app/lib/Screens/summary_screen.dart
@@ -24,6 +24,18 @@ class _SummaryScreenState extends State<SummaryScreen> {
     await flutterTts.speak(text);
   }
 
+  _getSummaryNote(){
+    String summary = "You have detected";
+    for (int note in notes) {
+      if(widget.res[note] != 0){
+        String word = widget.res[note] == 1? "note" : "notes";
+        summary += " ${widget.res[note]}|$note $word,";
+      }
+    }
+    print(summary);
+    return summary;
+    }
+
   _getTotal(){
     total = GetTotal.getTotal(widget.res);
   }
